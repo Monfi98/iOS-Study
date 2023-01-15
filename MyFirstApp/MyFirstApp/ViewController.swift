1e10eba0f9f82e445a7d2ee9f5933a5f91570c0f
shin seung jae <mok05247@naver.com>
2023-01-15 21:52:42 +0900

    add: MyFirstApp

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 신승재 on 2023/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
