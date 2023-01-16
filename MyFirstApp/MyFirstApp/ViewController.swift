0ef58cf082cc6c55d7d0859a8b92ded1c9ff722f
shin seung jae <mok05247@naver.com>
2023-01-16 10:24:52 +0900

    feat(MyFirstApp): 버튼 누르면 label속성 바꾸기

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func didTabButton(_ sender: Any) {
+        mainLabel.text = "안녕하세요"
+        mainLabel.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+        mainLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
 }
 
