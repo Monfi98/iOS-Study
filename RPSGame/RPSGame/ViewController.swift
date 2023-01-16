4a8b685e580931cbc211f0cf89ac6facab89311b
shin seung jae <mok05247@naver.com>
2023-01-16 15:36:21 +0900

    feat(RPSGame): 각 label, button 변수와 함수 만들기

--- RPSGame/RPSGame/ViewController.swift
+++ RPSGame/RPSGame/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapRPSButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didTapSelectButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didTapResetButton(_ sender: UIButton) {
+        
+    }
+    
 }
 
