8119c47128e83575ef29925d530032b402d1b8a0
shin seung jae <mok05247@naver.com>
2023-01-16 11:02:31 +0900

    create: DiceGame prj

---
+++ DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 신승재 on 2023/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
