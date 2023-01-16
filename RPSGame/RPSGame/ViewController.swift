a0ce4d30cda5eca3136a50b719dccebd6f156479
shin seung jae <mok05247@naver.com>
2023-01-16 15:17:52 +0900

    create: RPSGame prj

---
+++ RPSGame/RPSGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPSGame
+//
+//  Created by 신승재 on 2023/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
